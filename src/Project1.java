912ac4cdd8578fcfae7e53dad61d79c0730663a9
Kian <30553405+Lentinal@users.noreply.github.com>
2024-09-19 13:35:10 -0400

    Attempt5

--- src/Project1.java
+++ src/Project1.java
@@ -20,12 +20,6 @@ public class Project1 {
         ArrayList<States> statesList = new ArrayList<>();
         ArrayList<Transition> transitionsList = new ArrayList<>();
         ArrayList<String> wordList = new ArrayList<>();
-        ArrayList<Character> charList = new ArrayList<>();
-        ArrayList<Integer> originList = new ArrayList<>();
-        ArrayList<Integer> destinList = new ArrayList<>();
-        ArrayList<Integer> isFinal = new ArrayList<>();
-        ArrayList<Character> charTemp = new ArrayList<>();
-        ArrayList<Integer> currID = new ArrayList<>();
 
         //Input/Output
         Scanner scanner = new Scanner(new File("example.txt"));
@@ -37,224 +31,86 @@ public class Project1 {
             wordList.add(line);
 
         }
-
         //Sorts wordList
         Collections.sort(wordList);
+
+        for(String word : wordList) {
+            System.out.println(word);
+        }
 
         //Starter for .jff file
         head(printWriter);
 
-        int currentID = 1;
         int currentHeight = 0;
-        int currentWidth = 0;
-        char[] previousChars = {' '};
-        boolean branching = false;
-        int branches =0;
 
-        //Attempting to make a list of chars and then make all the states afterward
+        int stateCounter = 1;
+
+        //Goes over each word in wordList
         for (String word : wordList) {
-            System.out.println(word);
-            char[] chars = word.toCharArray();
+            int origin = 0; //Track working state
 
-            for (int j = 0; j < chars.length ; j++) {
+            for (int i = 0; i < word.length(); i++) {
+                char symbol = word.charAt(i);
+                int dest = 0; //Tracks destination state
 
-                //final == 0
-                //start of word == 1
-                //neither == 2
-                //3 used for initial p0
-                if (j == chars.length - 1) {
-
-                    isFinal.add(0);
-                } else if (j == 0) {
-                    isFinal.add(1);
-                } else {
-                    isFinal.add(2);
-                }
-
-                //Makes sure that the program doesn't blow up
-                //By making sure that it is only trying to compare in bounds
-                //Can be turned to try catch
-                if (j < previousChars.length - 1) {
-
-                        //Checks to see if the letter was already scanned before
-                        //If it did it would add the original and destin
-                        if (previousChars[j] == chars[j]) {
-                            charList.add(chars[j]);
-                            originList.add(originList.get(charList.size() - previousChars.length - j - 1));
-                            destinList.add(currentID);
-                            branching = true;
-
-                            //Checks to see if it is a new sequence
-                            //By checking to see if it is a different starting letter
-                            //and is at the beginning of a word
-                        } else if (previousChars[j] != chars[j] && j == 0) {
-                            charList.add(chars[j]);
-                            originList.add(0);
-                            destinList.add(currentID);
-
-                            //defaults adds any normal states
-                        } else {
-                            charList.add(chars[j]);
-                            originList.add(currentID-1);
-                            destinList.add(currentID);
-                            currentID++;
-                        }
-
-                        //Adds any normal states
-                } else {
-                    charList.add(chars[j]);
-                    originList.add(currentID-1);
-                    destinList.add(currentID);
-                    currentID++;
-
-                }
-
-                //Checks for concurrent letters
-                /*if(j!=0){
-                    if(chars[j] == chars[j-1]){
-                        charList.add(chars[j]);
-                        originList.add(currentID-1);
-                        destinList.add(currentID);
-                        currentID++;
-                    }
-                }*/
-
-
-            }
-            previousChars = word.toCharArray();
-
-        }
-
-        for (char c : charList) {
-            System.out.print(c);
-
-        }
-        System.out.println();
-
-        for (Integer origin : originList) {
-            System.out.print(origin + " ");
-        }
-        System.out.println(originList.size());
-
-        for (Integer i : destinList) {
-            System.out.print(i + " ");
-
-        }
-        System.out.println(destinList.size());
-
-        for (Integer i : isFinal) {
-            System.out.print(i + " ");
-        }
-        System.out.println(isFinal.size());
-
-        boolean start = true;
-        currentID = 1;
-
-        //Loops through charList array whilst stuff in array
-        while (!charList.isEmpty()) {
-            Character c = charList.removeFirst();
-            Integer origin = originList.removeFirst();
-            Integer dest = destinList.removeFirst();
-            int isFin = isFinal.removeFirst();
-
-            //When currentId equals the state it will create a new state
-            //Otherwise it should only create a transition
-            if (origin > 0 || start) {
-                start = false;
-                States state = new States(currentID, currentWidth, currentHeight, isFin, printWriter, c);
-                statesList.add(state);
-                currentID++;
-            }
-
-            switch (isFin) {
-                case (0): //When final
-                    Transition transition = new Transition(origin, dest, c, printWriter);
-                    transitionsList.add(transition);
-                    currentHeight += DIST_SCALE_VERT;
-                    currentWidth = 0;
-                    break;
-
-                case (1): //When Initial it will branch off centre
-                    transition = new Transition(origin, dest, c, printWriter);
-                    transitionsList.add(transition);
-                    currentWidth += DIST_SCALE_HOR;
-                    break;
-
-                case (2): //When in middle
-                    transition = new Transition(origin, dest, c, printWriter);
-                    transitionsList.add(transition);
-                    currentWidth += DIST_SCALE_HOR;
-                    break;
-
-            }
-
-
-        }
-
-        //Loops through whilst there is stuff in the scanner
-        /*while (!wordList.isEmpty()){
-            String line = wordList.removeFirst();
-
-            //Checks each char in scanned line
-            for (int i = 0; i < line.length(); i++) {
-                // Check if the current character is the end of the string
-                boolean isFinal = (i + 1 >= line.length()) || (line.charAt(i + 1) == '\r') || (line.charAt(i + 1) == '\n');
-                char symbol = line.charAt(i);
-
-                //Creates state for current character
-                States state = new States(currentID, i * DIST_SCALE_HOR, currentHeight, false, isFinal, printWriter, symbol);
-                statesList.add(state);
-
-                //Adds transitions
-                if(i==0) {
-                    Transition transition = new Transition(0, currentID, symbol, prevChar, i, printWriter);
-                    transitionsList.add(transition);
-                    currentID++;
-                } else{
-                    Transition transition = new Transition(currentID - 1, currentID, symbol, prevChar, i, printWriter);
-                    transitionsList.add(transition);
-                    currentID++;
-                }
-
-
-                //Checks for final state
-                if(isFinal){
-                    break;
-                }
-
-                //Checks for invalid chars taken from example
-                if (!Character.isLowerCase(symbol)) {
-                    System.out.println("Symbol '"+ symbol +"' is invalid. All characters should be from a-z");
+                //Checks for any non-lower case characters
+                //Taken from example
+                if(!Character.isLowerCase(symbol)){
+                    System.out.printf("Symbol '%c' is invalid. All characters should be from a-z%n", symbol);
                             System.exit(1); // Exit with code 1
                 }
-                if(prevChar != line.charAt(i)) {
-                    prevChar = line.charAt(i);
-                    transitionID = i;
+
+                //Checks if a transition from currentState with the input symbol already exists
+                for (Transition t : transitionsList) {
+                    if ((t.getOrigin() == origin) && (t.getSymbol() == symbol)) {
+                        dest = t.getDest() ;
+                        break;
+                    }
                 }
+
+                //If the transition does not exist, create a new state and add a transition
+                if (dest == 0) {
+                    dest = stateCounter;
+                    statesList.add(new States(dest, i * DIST_SCALE_HOR, currentHeight ,printWriter));
+                    transitionsList.add(new Transition(origin, dest, symbol,printWriter));
+                    stateCounter++;
+                }
+
+                origin = dest;  //Moves to the next state
             }
 
-            //Moves current height
-            currentHeight += DIST_SCALE_VERT;
-        }*/
+            //Sets if the state is final
+            // 0/null = default
+            // 1 = initial
+            // 2 = final
+            for (States s : statesList) {
+                if (s.getID() == (origin)) {
+                    s.setIsFinal(2);
+                    break;
+                }
+            }
+            currentHeight += 75;//Moves height down after each word
+        }
+        
 
         //Creates first state and set it to the front
-        States p0 = new States(0, -2 * DIST_SCALE_HOR, (currentHeight - DIST_SCALE_VERT) / 2, 3, printWriter, ' ');
+        States p0 = new States(0, -2 * DIST_SCALE_HOR, (currentHeight - DIST_SCALE_VERT) / 3, printWriter);
+        p0.setIsFinal(1);
         statesList.addFirst(p0);
+
+        //Starts and waits for all the stored State object/threads
         for (States state : statesList) {
             state.start();
             state.join();
         }
 
-        //DFA dfa = new DFA(transitionsList, statesList);
-        //dfa.start();
-        //dfa.join();
-
-
+        //Starts and waits for all the stored Transition object/threads
         for (Transition transition : transitionsList) {
             transition.start();
             transition.join();
         }
 
+        //Ends JFLAP file and closes all Input/Output
         tail(printWriter);
         printWriter.close();
         scanner.close();
@@ -285,18 +141,23 @@ class States extends Thread {
     private final int id;
     private final int x;
     private final int y;
-    private final char currChar;
-    private final int isFinal;
+    private int isFinal;
     private final PrintWriter printWriter;
 
-    public States(int id, int x, int y, int isFinal, PrintWriter printWriter, char currChar) {
+    public States(int id, int x, int y, PrintWriter printWriter) {
         this.id = id;
         this.x = x;
         this.y = y;
-        this.isFinal = isFinal;
         this.printWriter = printWriter;
-        this.currChar = currChar;
+        isFinal = 0;
+    }
 
+    public int getID(){
+        return id;
+    }
+
+    public void setIsFinal(int isFinal){
+        this.isFinal = isFinal;
     }
 
     @Override
@@ -310,11 +171,11 @@ class States extends Thread {
         printWriter.println("<x>" + x + "</x>");
         printWriter.println("<y>" + y + "</y>");
         switch (isFinal){
-            case (3) :
+            case (1) :
             System.out.println("<initial/>");
             printWriter.println("<initial/>");
                 break;
-            case (0) :
+            case (2) :
             System.out.println("<final/>");
             printWriter.println("<final/>");
                 break;
@@ -326,19 +187,11 @@ class States extends Thread {
         printWriter.println("</state>");
     }
 
-    public char getCurrChar() {
-        return currChar;
-    }
-
-    public int getID() {
-        return id;
-    }
-
 }
 
 class Transition extends Thread {
-    private int origin;
-    private int dest;
+    private final int origin;
+    private final int dest;
     private final char symbol;
     private final PrintWriter printWriter;
 
@@ -362,14 +215,6 @@ class Transition extends Thread {
         return symbol;
     }
 
-    public void setDest(int dest) {
-        this.dest = dest;
-    }
-
-    public void setOrigin(int origin) {
-        this.origin = origin;
-    }
-
     @Override
     public void run() {
         System.out.println("<transition>");
@@ -382,40 +227,5 @@ class Transition extends Thread {
         printWriter.println("<to>" + dest + "</to>");
         printWriter.println("<read>" + symbol + "</read>");
         printWriter.println("</transition>");
-    }
-}
-
-class DFA extends Thread {
-    private static ArrayList<Transition> transitions;
-    private static ArrayList<States> states;
-
-    public DFA(ArrayList<Transition> transitions, ArrayList<States> states) {
-        DFA.transitions = transitions;
-        DFA.states = states;
-
-    }
-
-    @Override
-    public void run() {
-        for (int i = 0; i < transitions.size(); i++) {
-            Transition tI = transitions.get(i);
-            for (int j = 1; j < transitions.size(); j++) {
-                Transition tJ = transitions.get(j);
-                if (tI.getOrigin() == tJ.getOrigin() && tI.getDest() != tJ.getDest() && tI.getSymbol() == tJ.getSymbol()) {
-                    tJ.setDest(tI.getDest());
-                }
-            }
-        }
-
-        for (int i = 0; i < states.size(); i++) {
-            States sI = states.get(i);
-            for (int j = 1; j < states.size(); j++) {
-                States sJ = states.get(j);
-                if (sI.getCurrChar() == sJ.getCurrChar() && sI.getID() != sJ.getID()) {
-
-                }
-            }
-        }
-
     }
 }
